c9a4b687e112f5028c5a3610dd657e8971918863
Ahmed Nadar <ahmedfay@hotmail.com>
2014-07-17 20:15:11 +0000

    Customize views for Author and Book

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -1,17 +1,8 @@
 ActiveAdmin.register Author do
+  sidebar "Books by this Author", only: :show do
+    table_for Book.joins(:author).where(author_id: author.id) do |table|
+      table.column("Title") { |book| book.name }
+      end
+    end
+  end
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
-end
--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,17 +1,19 @@
 ActiveAdmin.register Book do
-
+  index do
+    column "Title", :name
+    column :author, sortable: :author
+    column :genre, sortable: :genre
+    column :price, sortable: :price do |book|
+      number_to_currency book.price, unit: "$", separator: ",", delimiter: ",", format: "%u %n"
+    end
+    column "Created Date", :created_at
+    actions
+  end
   
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
+  filter :name
+  filter :author, :as => :check_boxes
+  filter :genre, :as => :check_boxes
+  filter :price
+  filter :created_at
+  filter :updated_at
 end
